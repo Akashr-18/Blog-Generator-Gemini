886ecd7e363354087f3e8db87663eebe97b40471
Akash R <115867523+Akashr-18@users.noreply.github.com>
2024-09-27 17:29:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ input = st.text_input("Enter the topic", key='input')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image=""   
 if uploaded_file is not None:
-    # Open the uploaded image
     image = Image.open(uploaded_file)
-    #To display the image
     st.image(image, caption="Uploaded Image (Resized)", use_column_width=True)
 
 button = st.button("Get Blog content")
